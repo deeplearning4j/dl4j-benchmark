7eb15afce82b513669fe20e03068e37dc308de5f
Alex Black <blacka101@gmail.com>
2018-06-12 12:09:29 +1000

    Add datatype config for profileSimpleBenchmark.sh

--- scripts/profileSimpleBenchmark.sh
+++ scripts/profileSimpleBenchmark.sh
@@ -12,13 +12,14 @@ cd dl4j-core-benchmark
 mkdir -p ../scripts/SimpleBenchmark
 
 #declare -a versionBackend=("v091_cuda8-cudnn" "v100beta_cuda8-cudnn")
-declare -a versionBackend=("v100beta_cuda91-cudnn")
-declare -a batchSize=("32")
+declare -a versionBackend=("v100snapshot_cuda91-cudnn")
+declare -a batchSize=("64")
+declare -a dataTypes=("FLOAT" "HALF")
 
 #model=ALEXNET
 model=RESNET50PRE
-xmx=12G
-javacpp=12G
+xmx=16G
+javacpp=16G
 
 # Launching with profiling:
 # https://www.yourkit.com/docs/java/help/agent.jsp
@@ -36,13 +37,16 @@ for i in "${versionBackend[@]}"
 do
    for j in "${batchSize[@]}"
    do
-      export SNAPSHOT_DIR=../scripts/SimpleBenchmark/$model/snapshot-$i-$j/
-      mkdir -p $SNAPSHOT_DIR
-      echo "Running test: $i, batch size $j"
-      echo java -agentpath:"$yourkitPath"=tracing,port=10001,dir=$SNAPSHOT_DIR,tracing_settings_path=$profilingSettingsPath -cp dl4j-core-benchmark-$i.jar  -Xmx$xmx -Dorg.bytedeco.javacpp.maxbytes=$javacpp -Dorg.bytedeco.javacpp.maxphysicalbytes=$javacpp org.deeplearning4j.simple.SimpleBenchmark --nIter 100 --fit --minibatch $j --model $model > ../scripts/SimpleBenchmark/$model/output_"$i"_"$j".txt
-      java -agentpath:"$yourkitPath"=tracing,port=10001,dir=$SNAPSHOT_DIR,tracing_settings_path=$profilingSettingsPath -cp dl4j-core-benchmark-$i.jar  -Xmx$xmx -Dorg.bytedeco.javacpp.maxbytes=$javacpp -Dorg.bytedeco.javacpp.maxphysicalbytes=$javacpp org.deeplearning4j.simple.SimpleBenchmark --nIter 100 --fit --minibatch $j --model $model >> ../scripts/SimpleBenchmark/$model/output_"$i"_"$j".txt
+      for k in "${dataTypes[@]}"
+      do
+          export SNAPSHOT_DIR=../scripts/SimpleBenchmark/$model/snapshot-$i-$j-$k/
+          mkdir -p $SNAPSHOT_DIR
+          echo "Running test: $i, batch size $j, data type $k"
+          echo java -agentpath:"$yourkitPath"=tracing,port=10001,dir=$SNAPSHOT_DIR,tracing_settings_path=$profilingSettingsPath -cp dl4j-core-benchmark-$i.jar  -Xmx$xmx -Dorg.bytedeco.javacpp.maxbytes=$javacpp -Dorg.bytedeco.javacpp.maxphysicalbytes=$javacpp org.deeplearning4j.simple.SimpleBenchmark --nIter 100 --fit --minibatch $j --model $model --datatype $k > ../scripts/SimpleBenchmark/$model/output_"$i"_"$j"_"$k".txt
+          java -agentpath:"$yourkitPath"=tracing,port=10001,dir=$SNAPSHOT_DIR,tracing_settings_path=$profilingSettingsPath -cp dl4j-core-benchmark-$i.jar  -Xmx$xmx -Dorg.bytedeco.javacpp.maxbytes=$javacpp -Dorg.bytedeco.javacpp.maxphysicalbytes=$javacpp org.deeplearning4j.simple.SimpleBenchmark --nIter 100 --fit --minibatch $j --model $model --datatype $k >> ../scripts/SimpleBenchmark/$model/output_"$i"_"$j"_"$k".txt
 
-      #Note: snapshot recording will automatically occur when JVM shuts down
+          #Note: snapshot recording will automatically occur when JVM shuts down
+      done
    done
 done
 
