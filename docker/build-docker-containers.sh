36d4b16010fc0cf964cf8805d4a57a9d5893ae7b
raver119 <raver119@gmail.com>
2019-07-18 15:34:03 +0300

    yml
    Signed-off-by: raver119 <raver119@gmail.com>

--- docker/build-docker-containers.sh
+++ docker/build-docker-containers.sh
@@ -8,5 +8,8 @@ docker build --no-cache -t dl4j-base:1.0.0 ./base
 # benchmark image: contains base dl4j image + dl4j-benchmarking jars
 docker build -t dl4j-benchmark-base:1.0.0 ../
 
-# memory benchmark base container
-docker build -t dl4j-benchmark-memory:1.0.0 ./benchmark-memory
+# dl4j memory benchmark container
+docker build -t dl4j-benchmark-memory-dl4j:1.0.0 ./benchmark-memory-dl4j
+
+# samediff memory benchmark container
+#docker build -t dl4j-benchmark-memory-sd:1.0.0 ./benchmark-memory-sd
