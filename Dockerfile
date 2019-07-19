6dfce6b055bb9b5000915b445fffa71573213cf1
raver119 <raver119@gmail.com>
2019-07-19 16:53:49 +0300

    better layout
    Signed-off-by: raver119 <raver119@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM dl4j-base:1.0.0 as builder
 
 COPY / /app/
 
-RUN cd /app && mvn -T 4 install -DskipTests=true -Dmaven.javadoc.skip=true -Dmaven.test.skip=true
+RUN cd /app && mvn install -DskipTests=true -Dmaven.javadoc.skip=true -Dmaven.test.skip=true -P v100snapshot,native -pl "!samediff-benchmark,!dl4j-core-benchmark"
--- docker/build-docker-containers.sh
+++ docker/build-docker-containers.sh
@@ -3,13 +3,16 @@
 set -e
 
 # base image: java, cpp and full DL4j stack
-docker build --no-cache -t dl4j-base:1.0.0 ./base
+docker build -t dl4j-linux-base:1.0.0 ./base
+
+# base DL4j image: stack
+docker build -t dl4j-base:1.0.0 ./deeplearning4j-base
 
 # benchmark image: contains base dl4j image + dl4j-benchmarking jars
 docker build -t dl4j-benchmark-base:1.0.0 ../
 
 # dl4j memory benchmark container
-docker build -t dl4j-benchmark-memory-dl4j:1.0.0 ./benchmark-memory-dl4j
+docker build -t dl4j-benchmark-memory-conv2d:1.0.0 ./benchmark-memory-conv2d
 
 # samediff memory benchmark container
 #docker build -t dl4j-benchmark-memory-sd:1.0.0 ./benchmark-memory-sd
