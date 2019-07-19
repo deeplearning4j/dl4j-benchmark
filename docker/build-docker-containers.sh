4230ebec26fbae575d1b82427fd18275bbee8fea
raver119 <raver119@gmail.com>
2019-07-19 18:47:17 +0300

    rnn model
    Signed-off-by: raver119 <raver119@gmail.com>

--- docker/build-docker-containers.sh
+++ docker/build-docker-containers.sh
@@ -11,8 +11,9 @@ docker build -t dl4j-base:1.0.0 ./deeplearning4j-base
 # benchmark image: contains base dl4j image + dl4j-benchmarking jars
 docker build -t dl4j-benchmark-base:1.0.0 ../
 
-# dl4j memory benchmark container
+# dl4j memory benchmark containers
 docker build -t dl4j-benchmark-memory-conv2d:1.0.0 ./benchmark-memory-conv2d
+docker build -t dl4j-benchmark-memory-rnn:1.0.0 ./benchmark-memory-rnn
 
 # samediff memory benchmark container
 #docker build -t dl4j-benchmark-memory-sd:1.0.0 ./benchmark-memory-sd
