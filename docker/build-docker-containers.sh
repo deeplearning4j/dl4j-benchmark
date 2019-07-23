3727806d23ea5030ac527d48200140864f36eacb
raver119 <raver119@gmail.com>
2019-07-23 10:18:35 +0300

    more containers
    Signed-off-by: raver119 <raver119@gmail.com>

--- docker/build-docker-containers.sh
+++ docker/build-docker-containers.sh
@@ -6,14 +6,15 @@ set -e
 docker build -t dl4j-linux-base:1.0.0 ./base
 
 # base DL4j image: stack
-docker build -t dl4j-base:1.0.0 ./deeplearning4j-base
+#docker build --no-cache -t dl4j-base:1.0.0 ./deeplearning4j-base
 
 # benchmark image: contains base dl4j image + dl4j-benchmarking jars
 docker build -t dl4j-benchmark-base:1.0.0 ../
 
 # dl4j memory benchmark containers
-docker build -t dl4j-benchmark-memory-conv2d:1.0.0 ./benchmark-memory-conv2d
-docker build -t dl4j-benchmark-memory-rnn:1.0.0 ./benchmark-memory-rnn
+docker build -t dl4j-benchmark-memory-conv2d:1.0.0 ./benchmark-memory-dl4j-conv2d
+docker build -t dl4j-benchmark-memory-rnn:1.0.0 ./benchmark-memory-dl4j-rnn
 
 # samediff memory benchmark container
-#docker build -t dl4j-benchmark-memory-sd:1.0.0 ./benchmark-memory-sd
+docker build -t sd-benchmark-memory-conv2d:1.0.0 ./benchmark-memory-sd-conv2d
+docker build -t sd-benchmark-memory-rnn:1.0.0 ./benchmark-memory-sd-rnn
